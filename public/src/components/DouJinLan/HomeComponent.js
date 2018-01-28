cd72b01ade0e7df51e149835aef7205ca9533438
doujinlan <610910710@qq.com>
2018-01-28 14:59:58 +0800

    change doujinlan

--- public/src/components/DouJinLan/HomeComponent.js
+++ public/src/components/DouJinLan/HomeComponent.js
@@ -6,6 +6,13 @@ export default class HomeComponent extends Component{
             <div className="homeComponent-contents-js homeComponent_contents_css">
                 <p>doujinlan</p>
                 <button>hello</button>
+                <ol>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ol>
             </div>
         )
     }
