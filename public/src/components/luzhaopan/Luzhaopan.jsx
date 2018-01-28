1600236c11cef0672f9255bd5bd2e2ba92e6c6cb
doujinlan <32731240+doujinlan@users.noreply.github.com>
2018-01-28 14:26:38 +0800

    Dev (#2)
    * change readme.md and doujinlan
    
    * 改了Test文件
    
    * 改了路招盘

---
+++ public/src/components/luzhaopan/Luzhaopan.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+export default class Test extends Component{
+    render(){
+        return(
+            <div>hell
+            luhaopan
+                luzhapan2
+                1111
+
+
+            </div>
+        )
+    }
+}
