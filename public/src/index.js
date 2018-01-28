503752948c6da057f82ec0c0f1805060c6157857
ls922 <2338737481@qq.com>
2018-01-28 14:46:12 +0800

    liushuo

--- public/src/index.js
+++ public/src/index.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import { Link } from 'react-router';
 import Test from './components/Test.jsx'
+import MyComponents from './MyComponents'
+
 
 
 class App extends Component {
     render() {
         return (
             <div className="aaa">
-                <button className="btn btn-default" type="submit">button---</button>
-                <Test/>
+               <MyComponents/>
             </div>
         )
     }
