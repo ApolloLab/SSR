3c8c662f3702e0391a9003570cab7fb156d1d000
doujinlan <32731240+doujinlan@users.noreply.github.com>
2018-01-28 13:55:42 +0800

    change readme.md and doujinlan (#1)

---
+++ public/src/components/DouJinLan/HomeComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class HomeComponent extends Component{
+    render(){
+        return(
+            <div className="homeComponent-contents-js homeComponent_contents_css">
+                <p>doujinlan</p>
+                <button>hello</button>
+            </div>
+        )
+    }
+}
