b55f5d178c6addbd506b5c586597df2509c58916
gegeqi <ccczzz1015@163.com>
2018-01-29 10:41:45 +0800

    nothing

--- public/src/index.js
+++ public/src/index.js
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
-import ReactDom from 'react-dom';
-import Test from './components/Test.jsx'
+import ReactDOM from 'react-dom';
+
+function App1(props) {
+    return <p>app1:{parseInt(props.number1) + 1}</p>
+}
+
+function App2(props) {
+    return (
+        <div>
+        <p>app2:{parseInt(props.number2) - 1}</p>
+        </div>
+)
+}
 
 
-class App extends Component {
+class Index extends React.Component {
+    constructor() {
+        super();
+        this.state = {number: ""}
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({number: e.target.value})
+    }
+
     render() {
         return (
-            <div className="aaa">
-                <button className="btn btn-default" type="submit">button---</button>
-                <Test/>
+            <div>
+                <input type="text" value={this.state.number} onChange={this.handleChange}/>
+                <App1 number1={this.state.number}/>
+                <App2 number2={this.state.number}/>
             </div>
         )
     }
 }
 
-ReactDom.render(
-    <App/>,
+
+ReactDOM.render(
+    <Index/>,
     document.getElementById('root')
-)
+);
