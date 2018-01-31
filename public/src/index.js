621802b9ca1ffb080ae155e7604cdb7459a787e2
ls922 <2338737481@qq.com>
2018-01-31 16:12:27 +0800

    add我的页面

--- public/src/index.js
+++ public/src/index.js
@@ -2,13 +2,14 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import LoginComponent from './components/qinqi/LoginComponent.js';
-
+import MyComponent from './components/liushuo/MyComponent.js';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <LoginComponent />
+                <MyComponent/>
             </div>
         )
     }
