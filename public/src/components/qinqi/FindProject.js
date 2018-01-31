c1392356a5ddaa475ddac9a208201175ce03c0e8
gegeqi <ccczzz1015@163.com>
2018-01-31 14:22:36 +0800

    秦齐 提交 router 和 登陆界面

---
+++ public/src/components/qinqi/FindProject.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class FindProject extends React.Component {
+    render () {
+        return (
+                <div style={{fontSize:"100px"}}>
+                    发现项目界面
+                </div>
+        );
+    }
+}
